fd2a349687e331a0bb1b72ec21fba949d314f81b
Kevin Buffardi <kbuffardi@csuchico.edu>
2018-11-01 15:59:15 -0700

    Updates current size at each function call

--- MyNixCLI.h
+++ MyNixCLI.h
@@ -8,22 +8,28 @@ class MyNixCLI
   private:
     struct winsize _window;
 
+    void update()
+    {
+      ioctl(STDOUT_FILENO, TIOCGWINSZ, &_window);
+    }
   public:
     //capture CLI window meta data upon declaration
     MyNixCLI()
     {
-      ioctl(STDOUT_FILENO, TIOCGWINSZ, &_window);
+      update();
     }
 
     //find how many monospace characters fit horizontally across columns
     int characterWidth()
     {
+      update();
       return _window.ws_col;
     }
 
     //find how many monospace characters fit vertically by row
     int characterHeight()
     {
+      update();
       return _window.ws_row;
     }
 };
